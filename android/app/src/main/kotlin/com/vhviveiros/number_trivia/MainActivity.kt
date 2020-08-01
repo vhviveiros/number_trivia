2da811b67e4dad4b7e8be549c522bee211b1da75
VICTOR HUGO DE SOUZA VIVEIROS <vh.viveiros@gmail.com>
2020-08-01 16:51:06 -0300

    added kiwi injection added ads (not working yet)

--- android/app/src/main/kotlin/com/vhviveiros/number_trivia/MainActivity.kt
+++ android/app/src/main/kotlin/com/vhviveiros/number_trivia/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.number_trivia
+package com.vhviveiros.number_trivia
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
---
+++ android/app/src/main/kotlin/com/vhviveiros/number_trivia/NumberTrivia.kt
@@ -0,0 +1,4 @@
+package com.vhviveiros.number_trivia
+
+class NumberTrivia {
+}
